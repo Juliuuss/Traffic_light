917c7830d6067a33d52565e87cb9a46dbe46abbe
Juliuuss <j.esp.ver@gmail.com>
2023-02-19 01:05:19 +0000

    Finalizado sin botones

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,25 @@
-import React from "react";
-
-//include images into your bundle
+import React, { useState } from "react";
 
 
-//create your first component
 const Home = () => {
+	const [color, setColor] = useState("");
+
 	return (
-		<div className="container">
-			<div className="red">
+		<div className="container d-flex">
+		<div className="container traffic-light ">
+			<div onClick={()=>setColor("red")} className={"red"+ (color==="red"? " glow": "" )}>
 
 			</div>
-			<div className="yellow">
+			<div onClick={()=>setColor("yellow")} className={"yellow"+ (color==="yellow"? " glow": "" )}>
 
 			</div>
-			<div className="green">
+			<div onClick={()=>setColor("green")} className={"green"+ (color==="green"? " glow": "" )}>
 
 			</div>
+		</div>
+		<div className="container d-flex">
+		<button type="button" class="btn btn-primary">Primary</button>
+		</div>
 		</div>
 	);
 };
